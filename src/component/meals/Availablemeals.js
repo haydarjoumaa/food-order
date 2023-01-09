6a6f7d8b079c1cb079a38ca28abb8401fe0ded55
haydar <haydarjoumaa@gmail.com>
2023-01-09 16:53:37 +0200

    adding backend firebase

--- src/component/meals/Availablemeals.js
+++ src/component/meals/Availablemeals.js
@@ -1,34 +1,47 @@
 import style from "./availabelmeal.module.css";
 import Card from "../UI/Card";
 import Mealitem from "./mealitem/Mealitem";
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 const Availabelmeal = () => {
+  const [DUMMY_MEALS, setmeals] = useState([]);
+  const [Loading, setLoading] = useState(false);
+  const [httperror, sethttperrror] = useState();
+  useEffect(() => {
+    setLoading(true);
+    const datafecth = async () => {
+      const fetchdata = await fetch(
+        "https://react-http-8cab7-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!fetchdata.ok) {
+        throw new Error("error here");
+      }
+      const data = await fetchdata.json();
+      let array = [];
+      for (let key in data) {
+        array.push({ id: key, ...data[key] });
+      }
+      return array;
+    };
+
+    datafecth()
+      .then((data) => {
+        setmeals(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log("hi");
+        sethttperrror(err);
+      });
+  }, []);
+
+  if (Loading) {
+    return <p>Loading</p>;
+  }
+  if (httperror) {
+    return <p>error in server</p>;
+  }
   return (
     <section className={style.meals}>
       <Card>
